4e909c6a27e7a31a680b7ad8fedeaff5ae1f5cdb
askat <saatbek493@gmail.com>
2024-09-11 20:34:11 +0300

    finished project

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import "./App.css";
+
+function header() {
+  return (
+    <div>
+       <div className="div-h11">
+        <h1 className='h11111'>Трикотожное платье в полоску</h1>
+      </div>
+      <div className='div-platye1'>
+        <img
+          className="img-platye1"
+          src="https://brusnikabrand.com/upload/resize_cache/iblock/809/1333_1999_1/rvpfyz5r0y12nivyw27dwbeqpd9gvwx4.jpg"
+          alt="platye"
+        />
+        <div>
+          <h4 className='colors'>Цвета в наличии</h4>
+          <div className="id1"></div>
+          <div className="id2"></div>
+          <div className="id3"></div>
+          <div className="id4"></div>
+        </div>
+        <div>
+          <h4 className='h41'> Размеры в наличии</h4>
+          <h4 className='h411'>XXL, XS, S, M, L, XL, XXS</h4>
+        </div>
+      </div>
+
+
+
+
+      <div className="div-h11">
+        <h1 className='h1111'>платье </h1>
+      </div>
+      <div className='div-platye1'>
+        <img
+          className="img-platye1"
+          src="https://ae01.alicdn.com/kf/Sca7442d6b17a4fc49b74ffe9aea77a7ao.jpg"
+          alt="platye"
+        />
+        <div>
+          <h4 className='colors1'>Цвета в наличии</h4>
+          <div className="id11"></div>
+          <div className="id21"></div>
+          <div className="id31"></div>
+          <div className="id41"></div>
+        </div>
+        <div>
+          <h4 className='h41'> Размеры в наличии</h4>
+          <h4 className='h411class'>XXL, XS, S, M, L, XL, XXS</h4>
+        </div>
+      </div> 
+
+      <div className="mers">
+        <h1>Платье для 1 плеча</h1>
+      </div>
+      <div className="rio">
+        <img
+          className="bmw"
+          src="https://hmonline.ru/pictures/product/middle/12623404_middle.jpg"
+          alt="platye1"
+        />
+      </div>
+      <div className="polo">
+        <h4>Цвета в наличии</h4>
+        <div className="div1"></div>
+        <div className="div2"></div>
+        <div className="div3"></div>
+        <div className="div4"></div>
+      </div>
+      <div>
+        <h4 className="kia">Размеры в наличии</h4>
+        <h4 className="toyota">XXL, XS, S, M, L, XL, XXS</h4>
+      </div>
+    </div>
+  );
+}
+
+export default header;
